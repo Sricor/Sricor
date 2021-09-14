fd6998a88d3f5fdc42f84cefcc30792da9823935
josricor <josricor@outlook.com>
2021-09-14 21:10:22 +0800

    music

---
+++ utils/NeteaseCloudMusic/index.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+import os
+import action
+
+infos = {
+    "phone": os.getenv('NETEASE_PHONE'),
+    "password": os.getenv('NETEASE_PASSWORD'),
+    # "sc_key": ["XXXX"],
+    # "tg_bot_key": ["XXXX", "XXXXX"],
+    "bark_key": os.getenv('BARK'),
+    # "wecom_key": ["XXXX", "XXXX", "XXXX"],
+    # "push_plus_key": ["XXXX"],
+    # "qmsg_key": ["XXXX"],
+    # "ding_token": ["XXXX"],
+}
+
+
+def main_handler(event, context):
+    action.tasks_pool(infos)
+
+
+if __name__ == "__main__":
+    main_handler("", "")
